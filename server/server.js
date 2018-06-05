882a46b89fb10d09dbcc32c5e8d98f22b13e4741
Ксения Платова <kseniya.platova@iss.digital>
2018-06-05 10:44:14 +0300

    Обертывание наитивного сервера в socket.io

--- server/server.js
+++ server/server.js
@@ -8,9 +8,13 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
-
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', () => {
+    console.log('io server connected');
+});
 
 server.listen(port, () => {
     console.log(`Server has been started on port: ${port}`);
