97a341ef3abe43db83a837f87edf24a0e69d0637
Ксения Платова <kseniya.platova@iss.digital>
2018-06-05 10:35:04 +0300

    Обернула express сервер в наитивный сервр

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const socketIO = require('socket.io');
 const path = require('path');
+const http =  require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server has been started on port: ${port}`);
 });
