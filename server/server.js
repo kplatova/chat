2dfb82a10bb32f0fa1cf8711dc3920b4e5849082
Ксения Платова <kseniya.platova@iss.digital>
2018-06-05 09:38:27 +0300

    Добавление в файл сервера пути к папке с front-частью

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+
+app.use(express.static(publicPath));
 
 app.listen(3000, () => {
     console.log('Server has been started');
