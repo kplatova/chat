8c5f6b08cbf13ec7e51631c0bfc7ab50e4e5c5ae
Ксения Платова <kseniya.platova@iss.digital>
2018-06-05 09:45:01 +0300

    Добавляю зависимость nodemon и настраиваю порт

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('Server has been started');
+app.listen(port, () => {
+    console.log(`Server has been started on port: ${port}`);
 });
