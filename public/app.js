009f4415f8e4a8327315145c44c748557434f171
Ксения Платова <kplatova@gmail.com>
2018-06-20 09:08:37 +0300

    Определение текущего польователя и отправка сообщений

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
 const socket = io();
 
 Vue.component('chat-message', {
-    props: ['message'],
+    props: ['message', 'user'],
     template: `
-        <div class="message">
+        <div class="message" :class="{owner: message.id === user.id}">
             <div class="message-content z-depth-1">
-                {{message.text}}
+                {{message.name}}: {{message.text}}
             </div>
         </div>
     `
@@ -15,12 +15,18 @@ new Vue({
     el: '#app',
     data: {
         message: '',
-        messages: []
+        messages: [],
+        user: {
+            name: '',
+            room: ''
+        }
     },
     methods: {
         sendMessage() {
             const message = {
-                text: this.message
+                text: this.message,
+                name: this.user.name,
+                id: this.user.id
             };
 
             socket.emit('message:create', message, error => {
@@ -37,7 +43,21 @@ new Vue({
             });
         }
     },
+    created() { // сразу при инициализации vue, до mounted()
+        const params = window.location.search.split('&');
+        const name = params[0].split('=')[1].replace('+', ' ');
+        const room = params[1].split('=')[1].replace('+', ' ');
+
+        this.user = {name, room}; // текущий юзер
+    },
     mounted() { // когда готов весь html
-        this.initConnection();
+        socket.emit('join', this.user, data => {
+            if(typeof data === 'string') {
+                console.error(data);
+            } else {
+                this.user.id = data.userId;
+                this.initConnection();
+            }
+        });
     }
 });
--- server/server.js
+++ server/server.js
@@ -10,19 +10,29 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const message = (name, text) => ({name, text});
+const message = (name, text, id) => ({name, text, id});
 
 app.use(express.static(publicPath));
 
 io.on('connection', socket => {
+    socket.on('join', (user, callback) => {
+        if(!user.name || !user.room) {
+            return callback('Введите номер комнаты и имя пользователя');
+        }
+
+        callback({userId: socket.id});
+
+        socket.emit('message:new', message('Admin', `Привет, ${user.name}`));
+    });
+
     socket.on('message:create', (data, callback) => {
         if (!data) {
             callback('Сообщение не может быть пустым');
         } else {
             callback();
-            io.emit('message:new', message('Admin', data.text));
+            io.emit('message:new', message(data.name, data.text, data.id));
         }
-    });
+    })
 });
 
 server.listen(port, () => {
